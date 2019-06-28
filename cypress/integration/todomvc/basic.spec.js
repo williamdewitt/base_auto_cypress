9aaaa4b2198d52e3eafc9b42c2e13916a473d44d
William de Witt <sasilverain@hotmail.com>
2019-06-28 13:12:54 +0200

    updates

---
+++ cypress/integration/todomvc/basic.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+// @ts-check
+
+describe('ToDO MVC', function() {
+    beforeEach(() => {
+        cy.visit('http://todomvc.com/');
+    });
+
+    it('works', () => {
+        
+    });
+})
